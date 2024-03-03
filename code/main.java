d941073519199a9fb1255ea7c3e482191f81b271
codekamlesh <142514652+codekamlesh@users.noreply.github.com>
2024-03-03 16:25:55 +0530

    Create main.java

---
+++ code/main.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class GuessTheNumber {
+    public static void main(String[] args) {
+        GameLogic game = new GameLogic();
+        game.startGame();
+    }
+}
